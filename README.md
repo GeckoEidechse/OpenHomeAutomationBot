676415e430c59b9c26f56b120e84e9c42bdefb15
GeckoEidechse <gecko.eidechse+git@pm.me>
2023-03-28 16:34:12 +0200

    Add basic scraper and crossposter
    Still missing lots of functionality like
    - scraping linked content
    - avoiding duplicates
    - only checking posts since last run
    - and much more

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # OpenHomeAutomationBot
 
+A reddit bot written in Python to crosspost reddit post matching some keywords from one subreddit to another
+
+## Usage
+
+Generate appropriate tokens for your reddit bot
+
+Create config file for [`praw`](https://praw.readthedocs.io/)
+
+```bash
+cat > praw.ini << EOF
+[openhomeautomationbot]
+client_id = YOUR_CLIENT_ID_HERE
+client_secret = YOUR_CLIENT_SECRET_HERE
+username = YOUR_USERNAME_HERE
+password = YOUR_PASSWORD_HERE
+user_agent = YOUR_USER_AGENTEOF_HERE
+EOF
+```
+
 This project uses [Poetry](https://python-poetry.org/). To run, [install Poetry](https://python-poetry.org/docs/#installation) then simply run:
 
 ```bash
--- openhomeautomationbot/main.py
+++ openhomeautomationbot/main.py
@@ -1,6 +1,66 @@
 from . import __version__
+from typing import List
 import logging
+import praw
 
+def check_for_keywords(text: str) -> bool:
+    # TODO regex and list in extra file
+    keywords = [
+        "open source",
+        "open-source",
+        "open",
+        "foss",
+        "floss",
+        "home assistant",
+        "home-assistant",
+        "homeassistant",
+    ]
+    # Check if text contains key words
+    return any(keyword in text.lower() for keyword in keywords)
+
+
+def check_if_fit_criteria(submission) -> bool:
+
+    # Get the contents of the title
+    title = submission.title
+
+    # First only check title
+    if check_for_keywords(title):
+        return True
+
+    # Get the contents of the post or URL, depending on the type of submission
+    if submission.is_self:
+        content = submission.selftext
+        if check_for_keywords(content):
+            return True
+
+    else:
+        content = submission.url
+        logging.warn("Content posts not yet supported")
+    False
+
+def scrape_subreddit(reddit, subreddit_name="homeautomation")-> List[praw.models.Submission]:
+    subreddit = reddit.subreddit(subreddit_name)
+
+    # Get last x posts
+    # TODO this should be based on timestamp since last visit instead
+    results = [submission for submission in subreddit.new(limit=20) if check_if_fit_criteria(submission)]
+
+    return results
+
+def crosspost_single_post(reddit: praw.Reddit, submission: praw.models.Submission, subreddit_name="o_homeautomation_test"):
+    target_subreddit = reddit.subreddit(subreddit_name)
+    crosspost_title = submission.title
+
+    crosspost = submission.crosspost(subreddit=target_subreddit, title=crosspost_title, send_replies=True)
+
+    logging.info(f"Crossposted to {crosspost.subreddit}: {crosspost.url}")
+
+def crosspost_posts(reddit: praw.Reddit, submissions: List[praw.models.Submission]):
+    for submission in submissions:
+        # TODO double check if already posted
+        crosspost_single_post(reddit, submission)
+        break # TODO only post single post for now
 
 def main():
     # Set log level
@@ -9,6 +69,14 @@ def main():
     logging.info("Hello, World!")
     logging.info(f"My package version is {__version__}")
 
+    # authenticate using praw.ini
+    reddit = praw.Reddit("openhomeautomationbot")
+
+    # Scrape posts and collect relevant ones
+    submissions = scrape_subreddit(reddit)
+
+    # Cross-post relevant posts
+    crosspost_posts(reddit, submissions)
 
 if __name__ == "__main__":
     main()
