3c3c1b251fac5d8b8f5590d87a42b077675f8a3a
GeckoEidechse <gecko.eidechse+git@pm.me>
2023-05-08 15:53:01 +0200

    refactor: Move filtering code to own file
    Functions used to filter content based on some criteria are moved to
    `filter.py`
    
    To clean up `main.py`

---
+++ openhomeautomationbot/filter.py
@@ -0,0 +1,58 @@
+import logging
+
+def check_for_keywords(text: str) -> bool:
+    """
+    Check if the given text contains any of the predefined keywords.
+
+    :param text: The text to check.
+    :type text: str
+    :return: True if the text contains any of the keywords, False otherwise.
+    :rtype: bool
+    """
+    # TODO regex and list in extra file
+    keywords = [
+        "open source",
+        "open-source",
+        "open",
+        "foss",
+        "floss",
+        "home assistant",
+        "home-assistant",
+        "homeassistant",
+    ]
+    # Check if text contains key words
+    return any(keyword in text.lower() for keyword in keywords)
+
+
+def check_if_fit_criteria(submission, latest_timestamp: float) -> bool:
+    """
+    Check if the given submission fits the criteria for cross-posting.
+
+    :param submission: The submission to check.
+    :type submission: praw.models.Submission
+    :param latest_timestamp: The timestamp of the latest post in the database.
+    :type latest_timestamp: float
+    :return: True if the submission fits the criteria, False otherwise.
+    :rtype: bool
+    """
+    # Get the contents of the title
+    title = submission.title
+
+    # Only consider new posts
+    if submission.created_utc <= latest_timestamp:
+        return False
+
+    # First only check title
+    if check_for_keywords(title):
+        return True
+
+    # Get the contents of the post or URL, depending on the type of submission
+    if submission.is_self:
+        content = submission.selftext
+        if check_for_keywords(content):
+            return True
+
+    else:
+        content = submission.url
+        logging.warn("Content posts not yet supported")
+    return False
--- openhomeautomationbot/main.py
+++ openhomeautomationbot/main.py
@@ -2,68 +2,12 @@ import os
 import json
 
 from openhomeautomationbot.database_management import read_database, update_database
+from openhomeautomationbot.filter import check_if_fit_criteria
 from openhomeautomationbot import __version__
 from typing import List
 import logging
 import praw
 
-
-def check_for_keywords(text: str) -> bool:
-    """
-    Check if the given text contains any of the predefined keywords.
-
-    :param text: The text to check.
-    :type text: str
-    :return: True if the text contains any of the keywords, False otherwise.
-    :rtype: bool
-    """
-    # TODO regex and list in extra file
-    keywords = [
-        "open source",
-        "open-source",
-        "open",
-        "foss",
-        "floss",
-        "home assistant",
-        "home-assistant",
-        "homeassistant",
-    ]
-    # Check if text contains key words
-    return any(keyword in text.lower() for keyword in keywords)
-
-
-def check_if_fit_criteria(submission, latest_timestamp: float) -> bool:
-    """
-    Check if the given submission fits the criteria for cross-posting.
-
-    :param submission: The submission to check.
-    :type submission: praw.models.Submission
-    :param latest_timestamp: The timestamp of the latest post in the database.
-    :type latest_timestamp: float
-    :return: True if the submission fits the criteria, False otherwise.
-    :rtype: bool
-    """
-    # Get the contents of the title
-    title = submission.title
-
-    # Only consider new posts
-    if submission.created_utc <= latest_timestamp:
-        return False
-
-    # First only check title
-    if check_for_keywords(title):
-        return True
-
-    # Get the contents of the post or URL, depending on the type of submission
-    if submission.is_self:
-        content = submission.selftext
-        if check_for_keywords(content):
-            return True
-
-    else:
-        content = submission.url
-        logging.warn("Content posts not yet supported")
-    return False
 
 
 def scrape_subreddit(
