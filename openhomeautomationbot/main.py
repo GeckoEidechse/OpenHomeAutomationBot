876928df83511ed688d0bbfdee7048103d8da782
GeckoEidechse <gecko.eidechse+git@pm.me>
2023-05-08 14:44:28 +0200

    Add db functionality to store posted submissions
    Stores posted submissions in a JSON file to keep track of previously
    x-posted submissions.
    
    This way we don't post duplicate posts

--- openhomeautomationbot/main.py
+++ openhomeautomationbot/main.py
@@ -1,3 +1,5 @@
+import os
+import json
 from openhomeautomationbot import __version__
 from typing import List
 import logging
@@ -28,17 +30,23 @@ def check_for_keywords(text: str) -> bool:
     return any(keyword in text.lower() for keyword in keywords)
 
 
-def check_if_fit_criteria(submission) -> bool:
+def check_if_fit_criteria(submission, latest_timestamp: float) -> bool:
     """
     Check if the given submission fits the criteria for cross-posting.
 
     :param submission: The submission to check.
     :type submission: praw.models.Submission
+    :param latest_timestamp: The timestamp of the latest post in the database.
+    :type latest_timestamp: float
     :return: True if the submission fits the criteria, False otherwise.
     :rtype: bool
     """
     # Get the contents of the title
     title = submission.title
+
+    # Only consider new posts
+    if submission.created_utc <= latest_timestamp:
+        return False
 
     # First only check title
     if check_for_keywords(title):
@@ -57,13 +65,17 @@ def check_if_fit_criteria(submission) -> bool:
 
 
 def scrape_subreddit(
-    reddit: praw.Reddit, subreddit_name: str = "homeautomation"
+    reddit: praw.Reddit,
+    latest_timestamp: float,
+    subreddit_name: str = "homeautomation",
 ) -> List[praw.models.Submission]:
     """
     Scrape the given subreddit for relevant posts.
 
     :param reddit: The Reddit instance to use for the API requests.
     :type reddit: praw.Reddit
+    :param latest_timestamp: The timestamp of the latest post in the database.
+    :type latest_timestamp: float
     :param subreddit_name: The name of the subreddit to scrape, defaults to "homeautomation".
     :type subreddit_name: str, optional
     :return: A list of relevant submissions.
@@ -76,7 +88,7 @@ def scrape_subreddit(
     results = [
         submission
         for submission in subreddit.new(limit=20)
-        if check_if_fit_criteria(submission)
+        if check_if_fit_criteria(submission, latest_timestamp)
     ]
 
     return results
@@ -123,6 +135,62 @@ def crosspost_posts(reddit: praw.Reddit, submissions: List[praw.models.Submissio
         crosspost_single_post(reddit, submission)
         break  # TODO only post single post for now
 
+def update_database(submissions: List[praw.models.Submission]):
+    """
+    Stores the list of newly posted posts in a database (just a JSON file)
+
+    Args:
+        submissions (List[praw.models.Submission]): A list of Submission objects to crosspost.
+
+    Returns:
+        None
+    """
+    database_file = "database.json"
+
+    # Load existing data from the database file
+    if os.path.exists(database_file):
+        with open(database_file, "r") as f:
+            data = json.load(f)
+    else:
+        data = {"posts": {}, "latest_timestamp": 0}
+
+    # Add new submissions to the data
+    for submission in submissions:
+        entry = {
+            "title": submission.title,
+            "url": submission.url,
+            "created_utc": submission.created_utc,
+        }
+        data["posts"][submission.id] = entry
+
+        # Update the latest timestamp if the current submission has a newer timestamp
+        if submission.created_utc > data["latest_timestamp"]:
+            data["latest_timestamp"] = submission.created_utc
+
+        # Add version field
+        data["version"] = 1
+
+    # Write updated data back to the database file
+    with open(database_file, "w") as f:
+        json.dump(data, f, indent=4)
+
+def read_database(database_file: str = "database.json") -> dict:
+    """
+    Reads the data from the JSON database file and returns it as a dictionary.
+
+    Args:
+        database_file (str, optional): The path to the database file. Defaults to "database.json".
+
+    Returns:
+        dict: A dictionary containing the data from the database file.
+    """
+    if os.path.exists(database_file):
+        with open(database_file, "r") as f:
+            data = json.load(f)
+    else:
+        data = {"posts": {}, "latest_timestamp": 0}
+
+    return data
 
 def main():
     """
@@ -143,11 +211,20 @@ def main():
     # authenticate using praw.ini
     reddit = praw.Reddit("openhomeautomationbot")
 
+    # Read data from the database
+    data = read_database()
+
+    # Get the latest timestamp from the database
+    latest_timestamp = data["latest_timestamp"]
+
     # Scrape posts and collect relevant ones
-    submissions = scrape_subreddit(reddit)
+    submissions = scrape_subreddit(reddit, latest_timestamp)
 
     # Cross-post relevant posts
     crosspost_posts(reddit, submissions)
+
+    # Add posts to database of past posts
+    update_database(submissions)
 
 
 if __name__ == "__main__":
